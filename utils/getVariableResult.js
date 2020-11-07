d7dd047ce42a8f8df9b9672b8856b16f9b8bd5c8
Naldson <naldson.bc@gmail.com>
2020-11-07 16:01:10 -0300

    adicionando personagens na geração de manchetes aleatórias

--- utils/getVariableResult.js
+++ utils/getVariableResult.js
@@ -2,6 +2,7 @@ const { Op } = require('sequelize')
 
 const Players = require('../database/models/players')
 const Teams = require('../database/models/teams')
+const Characters = require('../database/models/characters')
 
 const utils = require('../utils/headlineGeneratorUtil')
 
@@ -18,11 +19,14 @@ async function getVariableResult (variable) {
 
   const teamTable = ['_teamName_']
 
+  const characterTable = ['_character_']
+
   if (playerTable.includes(variable)) {
     const players = await Players.filterByVariable(variable)
     const player = utils.randomArray(players)
     return player.name
   }
+
   if (teamTable.includes(variable)) {
     const result = await Teams.findAll({
       where: { [Op.not]: { name: 'Fortaleza' } }
@@ -30,6 +34,13 @@ async function getVariableResult (variable) {
     const teams = result.map(r => r.dataValues)
     const team = utils.randomArray(teams)
     return team.name
+  }
+
+  if (characterTable.includes(variable)) {
+    const result = await Characters.findAll()
+    const characters = result.map(r => r.dataValues)
+    const character = utils.randomArray(characters)
+    return character.name
   }
 }
 
