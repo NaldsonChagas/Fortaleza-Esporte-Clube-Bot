566f9cde91d1ac601052352850ee2d6368e38d36
Naldson <naldson.bc@gmail.com>
2020-11-07 21:58:20 -0300

    função recursiva na geração de manchetes

--- controllers/headlineGeneratorController.js
+++ controllers/headlineGeneratorController.js
@@ -1,24 +1,8 @@
-const Headlines = require('../database/models/headlines')
-
-const utils = require('../utils/headlineGeneratorUtil')
-
-const getVariableResult = require('../utils/getVariableResult')
+const { generateHeadline } = require('../database/models/headlines')
 
 module.exports = {
   async index (req, res) {
-    const result = await Headlines.findAll()
-    const headlines = result.map(r => r.dataValues)
-
-    const sortedHeadline = utils.randomArray(headlines)
-    const variables = utils.getVariables(sortedHeadline.title)
-
-    let headline = sortedHeadline.title
-
-    for (const variable of variables) {
-      const result = await getVariableResult(variable)
-      headline = headline.replace(variable, result)
-    }
-
+    const headline = await generateHeadline()
     return res.json({ status: 'ok', result: headline })
   }
 }
--- database/models/headlines.js
+++ database/models/headlines.js
@@ -1,6 +1,9 @@
 const connection = require('../config')
 const Sequelize = require('sequelize')
 
+const utils = require('../../utils/headlineGeneratorUtil')
+const getVariableResult = require('../../utils/getVariableResult')
+
 const Headlines = connection.define('headlines', {
   title: {
     type: Sequelize.TEXT,
@@ -26,6 +29,26 @@ Headlines.save = async (data) => {
   const headline = await Headlines.create({ title })
 
   return headline
+}
+
+Headlines.generateHeadline = async () => {
+  try {
+    const result = await Headlines.findAll()
+    const headlines = result.map(r => r.dataValues)
+
+    const sortedHeadline = utils.randomArray(headlines)
+    const variables = utils.getVariables(sortedHeadline.title)
+
+    let headline = sortedHeadline.title
+    for (const variable of variables) {
+      const result = await getVariableResult(variable)
+      headline = headline.replace(variable, result)
+    }
+    return headline
+  } catch {
+    console.log('Trying generate new headline')
+    return await Headlines.generateHeadline()
+  }
 }
 
 module.exports = Headlines
