31c82b794dbf9826bd0a6659e49383da2b400233
Naldson Chagas <naldson.bc@gmail.com>
2022-06-18 21:17:56 -0300

    testing player service

--- src/player/player.service.spec.ts
+++ src/player/player.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Player } from './player';
 import { PlayerService } from './player.service';
 
 describe('PlayerService', () => {
@@ -6,6 +9,7 @@ describe('PlayerService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, ConfigModule],
       providers: [PlayerService],
     }).compile();
 
@@ -14,5 +18,34 @@ describe('PlayerService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Test get all players methot', () => {
+    it('Should reurn all players', async () => {
+      const players: Player[] = [
+        {
+          name: 'Player 1',
+          isFortalezaPlayer: true,
+          isIdol: true,
+          isIconic: true,
+          isCurrentFortalezaPlayer: true,
+        },
+        {
+          name: 'Player 2',
+          isFortalezaPlayer: true,
+          isIdol: true,
+          isIconic: true,
+          isCurrentFortalezaPlayer: true,
+        },
+      ];
+
+      const result: Promise<Player[]> = new Promise((resolve) =>
+        resolve(players),
+      );
+
+      jest.spyOn(service, 'getPlayers').mockImplementation(() => result);
+
+      expect(await service.getPlayers()).toBe(await result);
+    });
   });
 });
